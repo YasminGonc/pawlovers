f664315453331c5c6de276a0c1b28794ecf7f672
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-08-05 14:57:11 -0300

    página 2

--- cadastropet/cadastropet.js
+++ cadastropet/cadastropet.js
@@ -1,3 +1,11 @@
 const dados = JSON.parse(localStorage.getItem('dados'));
+const nomeUsuario = document.querySelector('#nome-usuario');
 
-console.log(dados);
+function mostrarNome() {
+    const ultimoDadoCadastrado = (dados.length) - 1;
+    nomeUsuario.innerText = dados[ultimoDadoCadastrado].nome;
+}
+
+mostrarNome();
+
+const radios = document.querySelectorAll('.radios');
