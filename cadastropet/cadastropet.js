490399bb42e240295a9bbc05f058aa763c70d813
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-08-06 14:42:53 -0300

    adequações js

--- cadastropet/cadastropet.js
+++ cadastropet/cadastropet.js
@@ -11,21 +11,27 @@ mostrarNome();
 const radios = document.querySelectorAll('.radios');
 const div = document.querySelectorAll('.formulario__opcoes-card');
 const btnNext = document.querySelector('#next');
-const form = document.querySelector('.formulario__opcoes');
+const form = document.querySelector('.form');
+
+form.addEventListener('click', () => {
+    for(let i = 0; i < radios.length; i++) {
+        div[i].style.backgroundColor = 'transparent';
+
+        if(radios[i].checked == true) {
+            div[i].style.backgroundColor = '#F1CFCD';
+        }
+    }
+});
 
 btnNext.addEventListener('click', (evento) => {
     
-    for(let i = 0; i < radios.length; i++) {
-        if(radios[i].checked != true) {
-            console.log('false');
-            evento.preventDefault();
+    for (let j = 0; j < radios.length; j++) {
+        if (radios[j].checked == true){
+            break;
         }
         else {
-            console.log('true');
+            evento.preventDefault();
         }
-
     }
-    
-})
-
+});
 
